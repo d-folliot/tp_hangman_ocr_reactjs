170401de619ca69160dcb4674ed62c5717da8287
d-folliot <53329638+d-folliot@users.noreply.github.com>
2020-08-10 14:52:41 +0200

    version barebone (sans ajouts proposés)
    wordlist avec mot selectionné au hasard et currentword créé à la volée.
    Lissage de l'usage du this dans les composants reacts pour opti

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
 
+
+const DEFAULTKEYLIST = Array.from(Array(26).keys()).map(( y) => { return {keyname : String.fromCharCode(y + 65), used : false}});   
+const WORDLIST = ['Finances', 'Mince', 'Almanach', 'Cabane', 'Grenade', 'Noisette', 'Cadeau', 'Sourcils', 'Drapeaux', 'Construire', 
+'Desserrer', 'Impulsion','Volaille','Caresse','Couvent', 'Cravate','Excursion', 'Vitrine', 'Blessure'];
+
 function LetterKey(props){
   return <div type="submit" className={"letterkey " + (props.used?"used" : "unused")} onClick={props.onClick} >{props.keyname}</div>;
 }
@@ -9,54 +14,60 @@ function Keyboard(props){
   return <div className="keyboard" >
   {
     props.keylist.map( 
-    (key) =><LetterKey keyname={key.keyname} used={key.used} key={key.keyname} onClick={() => props.onInteraction(key.keyname)}/> )
+    (key) =><LetterKey keyname={key.keyname} used={key.used} key={key.keyname} onClick={() => props.onClick(key.keyname)}/> )
   }
   </div> ;
 }
 
 function Hangman(props){
-  return <div className="hangman">
-
-  </div>;
+  return <div className="hangman"></div>;
 }
 
-function Letter(props){
-  return <div className="letter">
-    {props.letter}
-  </div>;
-}
 
-function Word (props){
+function Word ({word}){
   return <div className="word">
-    {props.word.split("").map((letter, index) => <Letter letter={letter} key={index}/> )}
-  </div>
+      {word.split("").map((letter, index) => <div className="letter" key={index}>{letter}</div>  )}
+    </div>
 }
 
 
 class App extends React.Component {
   constructor(props){
     super(props);
-    let keylist = Array.from(Array(26).keys()).map(( y) => { return {keyname : String.fromCharCode(y + 65), used : false}});
-    console.log(keylist);
-    this.state = {keylist : keylist, currentword:"__________" ,finalword: "MACHINTRUC"};
+    this.state = {keylist : DEFAULTKEYLIST, currentword:" " ,finalword: ""};    
+  }  
+
+  componentDidMount(){
+    this.newgame();
   }
 
-  keyhit(key){    
+  //utilisation de this 
+  newgame = () =>{  
+    let finalword= WORDLIST[Math.floor(Math.random() * WORDLIST.length)].toUpperCase();
+    let currentword = (finalword.split("").map(letter => '_')).join("");
+    this.setState({finalword:finalword, currentword:currentword, keylist : DEFAULTKEYLIST});
+  }
+
+  // utilisation de this
+  keyhit = key =>{    
     let keylist=this.state.keylist.map((item) => { return{ keyname: item.keyname, used : (item.keyname===key)? true : item.used }});
     let currentword = this.state.currentword.split("");
     for (let i=0; i<currentword.length; i++){
       if (this.state.finalword[i]=== key)
         currentword[i] = key;
     }
-
     this.setState({keylist:keylist, currentword: currentword.join("")});
-
   }
+
   render() {
     return <div className="wrapper">
       <Hangman/>
       <Word word={this.state.currentword}/>
-      <Keyboard keylist={this.state.keylist} onInteraction={(key)=>this.keyhit(key)}/> 
+      {
+        this.state.currentword ===this.state.finalword ? 
+          <div className="newgamediv"><p className="newgametxt">Félicitation!</p><button onClick={this.newgame} className="newgame"> Nouvelle partie </button></div>
+          : <Keyboard keylist={this.state.keylist} onClick={this.keyhit}/> 
+      }
       <footer></footer>
       </div>
   }
